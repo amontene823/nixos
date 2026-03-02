18687da9292a92978acc16a288fab6d4457da708
Angelo Montenegro <amontene823@gmail.com>
2026-03-02 00:35:20 -0500

    added crlf/eol to HM git config

--- home/angelo/git.nix
+++ home/angelo/git.nix
@@ -4,23 +4,32 @@
   programs.git = {
     enable = true;
 
-    userName = "Angelo Montenegro";
-    userEmail = "amontene823@gmail.com";
+    settings = {
+      user = {
+        name = "Angelo Montenegro";
+        email = "amontene823@gmail.com";
+      };
 
-    extraConfig = 
       init.defaultBranch = "main";
       pull.rebase = false;
       push.autoSetupRemote = true;
       fetch.prune = true;
       rerere.enabled = true;
-    };
 
-    aliases = {
-      st = "status";
-      co = "checkout";
-      br = "branch";
-      ci = "commit";
-      lg = "log --oneline --decorate --graph --all";
+      alias = {
+        st = "status";
+        co = "checkout";
+        br = "branch";
+        ci = "commit";
+        lg = "log --oneline --decorate --graph --all";
+      };
+
+      # Line ending policy for Linux
+      core = {
+        autocrlf = "input";
+        eol = "lf";
+        safecrlf = "warn";
+      };
     };
   };
 }
